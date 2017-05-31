af60969183af3f9ad17e75fbacc617fa9d18dd84
binarykittenDE <franziska.haaf@gmail.com>
2017-05-31 09:21:18 +0200

    downgrad from es6

--- index.js
+++ index.js
@@ -10,35 +10,40 @@ restService.use(bodyParser.json());
 /**
  * Example Recipe ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
  */
-let oil = new Ingredient('oil', '5 tablespoons');
-let chocolate = new Ingredient('dark chocolate (vegan)', '200 gram');
-let flour = new Ingredient('flour', '170 gram');
-let cocoaPowder = new Ingredient('cocoa powder', '3 teaspoons');
-let sugar = new Ingredient('sugar', '180 gram');
-let seaSalt = new Ingredient('sea salt', 'a pinch');
-let vanillaPod = new Ingredient('vanilla pod', '1');
-let veganMilk = new Ingredient('vegan milk', '230 milliliter');
-let walnuts = new Ingredient('walnuts', '200 gram');
-let ingredients = [oil, chocolate, flour, cocoaPowder, sugar, seaSalt, vanillaPod, veganMilk, walnuts];
+var oil = {name: 'oil', amount: '5 tablespoons'};
+var chocolate = {name: 'dark chocolate (vegan)', amount: '200 gram'};
+var flour = {name: 'flour', amount: '170 gram'};
+var cocoaPowder = {name: 'cocoa powder', amount: '3 teaspoons'};
+var sugar = {name: 'sugar', amount: '180 gram'};
+var seaSalt = {name: 'sea salt', amount: 'a pinch'};
+var vanillaPod = {name: 'vanilla pod', amount: '1'};
+var veganMilk = {name: 'vegan milk', amount: '230 milliliter'};
+var walnuts = {name: 'walnuts', amount: '200 gram'};
+var ingredients = [oil, chocolate, flour, cocoaPowder, sugar, seaSalt, vanillaPod, veganMilk, walnuts];
 
-let step1 = 'So first preheat the oven to 180 degrees and cover an oven tray with baking paper.';
-let step2 = 'The next step would be to heat up 150 gram of chocolate until its completely melted. You can do this' +
+var step1 = 'So first preheat the oven to 180 degrees and cover an oven tray with baking paper.';
+var step2 = 'The next step would be to heat up 150 gram of chocolate until its compvarely melted. You can do this' +
     ' with an microwave or a waterbath.';
-let step3 = 'Sieve the 170 gram flour and 5 teaspoons cocoa powder into a large bowl.';
-let step4 = 'Add the 180 gram of sugar and a pinch of the sea salt.';
-let step5 = 'Halve the vanilla pod lengthways, scrape out the seeds and add them to the bowl.';
-let step6 = 'Stir in the 5 tablespoons oil, the 230 milliliters vegan milk and the melted chocolate until everything' +
+var step3 = 'Sieve the 170 gram flour and 5 teaspoons cocoa powder into a large bowl.';
+var step4 = 'Add the 180 gram of sugar and a pinch of the sea salt.';
+var step5 = 'Halve the vanilla pod lengthways, scrape out the seeds and add them to the bowl.';
+var step6 = 'Stir in the 5 tablespoons oil, the 230 milliliters vegan milk and the melted chocolate until everything' +
     ' is combined nicely.';
-let step7 = 'Roughly chop the remaining 50 gram of chocolate and 150 gram of walnuts, and stir it in!';
-let step8 = 'Wow, we nearly finished! Pour the mixture into the prepared oven tray and spread it out evenly.' +
+var step7 = 'Roughly chop the remaining 50 gram of chocolate and 150 gram of walnuts, and stir it in!';
+var step8 = 'Wow, we nearly finished! Pour the mixture into the prepared oven tray and spread it out evenly.' +
     ' Sprinkle over the remaining walnuts and place this beauty in the hot oven! I´ll remind you in 20 minutes.';
-let steps = [step1, step2, step3, step4, step5, step6, step7, step8];
+var steps = [step1, step2, step3, step4, step5, step6, step7, step8];
 //recipe
-let chocolateBrownies = new Recipe('chocolate brownies', ingredients, steps, 20);
+var chocolateBrownies = {
+    name: 'chocolate brownies',
+    ingredients: ingredients,
+    steps: steps,
+    timerTime: 20
+};
 //counter
-let currentStep = 0;
+var currentStep = 0;
 function getNextStep() {
-    let nextStep = currentStep++;
+    var nextStep = currentStep++;
     if (nextStep < chocolateBrownies.steps.length) {
         currentStep++;
         return chocolateBrownies.steps[nextStep]
@@ -48,7 +53,7 @@ function getCurrentStep() {
     return chocolateBrownies.steps[currentStep];
 }
 function getLastStep() {
-    let lastStep = currentStep--;
+    var lastStep = currentStep--;
     if (lastStep >= 0) {
         currentStep--;
         return chocolateBrownies.steps[lastStep]
@@ -62,10 +67,10 @@ restService.post('/hook', function (req, res) {
     console.log('hook request');
 
     try {
-        let speech = 'empty speech';
+        var speech = 'empty speech';
 
         if (req.body) {
-            let requestBody = req.body;
+            var requestBody = req.body;
 
             if (requestBody.result) {
                 speech = '';
